efdcebda4eaa96cad75ef7302951dbd82db1d79a
teorth <tao@math.ucla.edu>
2023-12-26 09:16:39 -0800

    Use nonzero constants

--- Asymptotic/majorize.lean
+++ Asymptotic/majorize.lean
@@ -8,12 +8,21 @@ variable {E : Type*} [SeminormedAddCommGroup E]
 variable {V : Type*} [SeminormedAddCommGroup V] [NormedSpace ℝ V]
 variable {k : Type*} [NormedField k]
 
-def Ll (C:NNReal) (X:E) (Y:ℝ) : Prop := ‖X‖ ≤ C * Y
+/-- The requirement that C is non-zero is convenient as it forces Y to be nonnegative, without having to explicitly place Y in NNReal. -/
+def Ll (C:NNReal) [NeZero C] (X:E) (Y:ℝ) : Prop := ‖X‖ ≤ C * Y
 
 notation:10 X " ≪[" C "] " Y => Ll C X Y
 
 @[simp]
-lemma ll_def (C:NNReal) (X:E) (Y:ℝ) : (X ≪[C] Y) ↔ (‖X‖ ≤ C * Y) := by rfl
+lemma ll_def (C:NNReal) [NeZero C] (X:E) (Y:ℝ) : (X ≪[C] Y) ↔ (‖X‖ ≤ C * Y) := by rfl
+
+lemma nonneg_of_ll {C:NNReal} [NeZero C] {X:E} {Y:ℝ} (h: X ≪[C] Y) : 0 ≤ Y  := by
+  simp at h
+  replace h := (norm_nonneg X).trans h
+  have : 0 < C := NeZero.pos C
+  rw [<-mul_le_mul_left (a := (C:ℝ))]
+  . simpa
+  norm_cast
 
 lemma ll_of_le {X:E} {Y:ℝ} (h: ‖X‖ ≤ Y) : X ≪[1] Y := by simp [h]
 
@@ -21,87 +30,104 @@ lemma ll_abs (X:E) : X ≪[1] ‖X‖ := by simp
 
 lemma ll_refl {X:ℝ} (hX: 0 ≤ X) : X ≪[1] X := by simp [abs_of_nonneg hX]
 
-lemma norm_ll_iff (C:NNReal) (X:E) (Y:ℝ) : (X ≪[C] Y) ↔ (‖X‖ ≪[C] Y) := by
+lemma norm_ll_iff (C:NNReal) [NeZero C] (X:E) (Y:ℝ) : (X ≪[C] Y) ↔ (‖X‖ ≪[C] Y) := by
   simp
 
-lemma ll_move_const {C₁:NNReal} {C₂:ℝ} (hC₂: 0 ≤ C₂) {X:E} (Y:ℝ) : (X ≪[C₁] C₂*Y) ↔ (X ≪[C₁*C₂.toNNReal] Y) := by
-  simp [hC₂]; rw [mul_assoc]
-
-lemma ll_trans {C₁ C₂:NNReal} {X:E} {Y Z:ℝ} (hY: 0 ≤ Y) (hXY: X ≪[C₁] Y) (hYZ: Y ≪[C₂] Z) : X ≪[C₁*C₂] Z := by
-  simp [abs_of_nonneg hY] at hXY hYZ ⊢
+lemma ll_trans {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {X:E} {Y Z:ℝ} (hXY: X ≪[C₁] Y) (hYZ: Y ≪[C₂] Z) : X ≪[C₁*C₂] Z := by
+  simp [abs_of_nonneg (nonneg_of_ll hXY)] at hXY hYZ ⊢
   apply hXY.trans
   rw [mul_assoc]
   exact mul_le_mul_of_nonneg_left hYZ (NNReal.coe_nonneg C₁)
 
-lemma ll_of_ll_of_le {C:NNReal} {X:E} {Y Z:ℝ} (hXY: X ≪[C] Y) (hYZ: Y ≤ Z) : X ≪[C] Z := by
+lemma ll_of_ll_of_le {C:NNReal} [NeZero C] {X:E} {Y Z:ℝ} (hXY: X ≪[C] Y) (hYZ: Y ≤ Z) : X ≪[C] Z := by
   simp at hXY ⊢
   apply hXY.trans
   exact mul_le_mul_of_nonneg_left hYZ (NNReal.coe_nonneg C)
 
-lemma ll_of_le_of_ll {C:NNReal} {X:E} {Y Z:ℝ} (hXY: ‖X‖ ≤ Y) (hYZ: Y ≪[C] Z) : X ≪[C] Z := by
-  have : 0 ≤ Y := le_trans (norm_nonneg X) hXY
-  convert ll_trans this (ll_of_le hXY) hYZ
+lemma ll_of_le_of_ll {C:NNReal} [NeZero C] {X:E} {Y Z:ℝ} (hXY: ‖X‖ ≤ Y) (hYZ: Y ≪[C] Z) : X ≪[C] Z := by
+  convert ll_trans (ll_of_le hXY) hYZ
   exact (one_mul C).symm
 
-lemma ll_of_le_of_ll' {C:NNReal} {X Y Z:ℝ} (hX: 0 ≤ X) (hXY: X ≤ Y) (hYZ: Y ≪[C] Z) : X ≪[C] Z := by
+lemma ll_of_le_of_ll' {C:NNReal} [NeZero C] {X Y Z:ℝ} (hX: 0 ≤ X) (hXY: X ≤ Y) (hYZ: Y ≪[C] Z) : X ≪[C] Z := by
   apply ll_of_le_of_ll _ hYZ
   simp [abs_of_nonneg hX, hXY]
 
-lemma ll_increase_C {C₁ C₂:NNReal} {X:E} {Y:ℝ} (hY: 0 ≤ Y) (hXY : X ≪[C₁] Y) (hC : C₁ ≤ C₂) : X ≪[C₂] Y := hXY.trans (mul_le_mul_of_nonneg_right hC hY)
+lemma ll_increase_const {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {X:E} {Y:ℝ} (hXY : X ≪[C₁] Y) (hC : C₁ ≤ C₂) : X ≪[C₂] Y := hXY.trans (mul_le_mul_of_nonneg_right hC (nonneg_of_ll hXY))
 
-lemma neg_ll {C: NNReal} {X:E} (Y:ℝ) : (X ≪[C] Y) ↔ ((-X) ≪[C] Y) := by
+lemma ll_absorb_const {C₁ C₃:NNReal} [NeZero C₁] [NeZero C₃] {C₂:ℝ} (hC₂: 0 < C₂) (hC: C₁ * C₂ ≤ C₃) {X:E} (Y:ℝ) (hXY: X ≪[C₁] C₂*Y) : X ≪[C₃] Y := by
+  have := nonneg_of_ll hXY
+  simp at hXY ⊢
+  apply hXY.trans
+  rw [<-mul_assoc]
+  apply mul_le_mul_of_nonneg_right hC
+  rw [<-mul_le_mul_left (a := (C₂:ℝ))]
+  . simpa
+  norm_cast
+
+lemma neg_ll {C: NNReal} [NeZero C] {X:E} (Y:ℝ) : (X ≪[C] Y) ↔ ((-X) ≪[C] Y) := by
   simp
 
-lemma add_ll {C₁ C₂:NNReal} {X₁ X₂:E} (Y:ℝ) (h1: X₁ ≪[C₁] Y) (h2: X₂ ≪[C₂] Y) : X₁ + X₂ ≪[C₁ + C₂] Y := by
+instance {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] : NeZero (C₁ + C₂) where
+  out := zero_lt_iff.mp (add_pos (NeZero.pos C₁) (NeZero.pos C₂))
+
+lemma add_ll {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {X₁ X₂:E} (Y:ℝ) (h1: X₁ ≪[C₁] Y) (h2: X₂ ≪[C₂] Y) : X₁ + X₂ ≪[C₁ + C₂] Y := by
   simp at h1 h2 ⊢
   apply (norm_add_le X₁ X₂).trans
   convert add_le_add h1 h2 using 1
   ring
 
-lemma add_ll_add {C₁ C₂:NNReal} {X₁ X₂:E} {Y₁ Y₂:ℝ} (hY₁: 0 ≤ Y₁) (hY₂: 0 ≤  Y₂) (h1: X₁ ≪[C₁] Y₁) (h2: X₂ ≪[C₂] Y₂) : X₁ + X₂ ≪[max C₁ C₂] Y₁ + Y₂ := by
+instance {C₁ C₂:NNReal} [NeZero C₁] : NeZero (max C₁ C₂) where
+  out := zero_lt_iff.mp (lt_max_of_lt_left (NeZero.pos C₁))
+
+lemma add_ll_add {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {X₁ X₂:E} {Y₁ Y₂:ℝ} (h1: X₁ ≪[C₁] Y₁) (h2: X₂ ≪[C₂] Y₂) : X₁ + X₂ ≪[max C₁ C₂] Y₁ + Y₂ := by
   simp at h1 h2 ⊢
   apply (norm_add_le X₁ X₂).trans
   apply (add_le_add h1 h2).trans
   rw [mul_add]
   apply add_le_add
-  . exact mul_le_mul_of_nonneg_right (le_max_left C₁ C₂) hY₁
-  exact mul_le_mul_of_nonneg_right (le_max_right C₁ C₂) hY₂
+  . exact mul_le_mul_of_nonneg_right (le_max_left C₁ C₂) (nonneg_of_ll h1)
+  exact mul_le_mul_of_nonneg_right (le_max_right C₁ C₂) (nonneg_of_ll h2)
 
-lemma sub_ll {C₁ C₂:NNReal} {X₁ X₂:E} (Y:ℝ) (h1: X₁ ≪[C₁] Y) (h2: X₂ ≪[C₂] Y) : X₁ - X₂ ≪[C₁ + C₂] Y := by
+lemma sub_ll {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {X₁ X₂:E} (Y:ℝ) (h1: X₁ ≪[C₁] Y) (h2: X₂ ≪[C₂] Y) : X₁ - X₂ ≪[C₁ + C₂] Y := by
   simp at h1 h2 ⊢
   apply (norm_sub_le X₁ X₂).trans
   convert add_le_add h1 h2 using 1
   ring
 
-lemma sub_ll_add {C₁ C₂:NNReal} {X₁ X₂:E} {Y₁ Y₂:ℝ} (hY₁: 0 ≤ Y₁) (hY₂: 0 ≤  Y₂) (h1: X₁ ≪[C₁] Y₁) (h2: X₂ ≪[C₂] Y₂) : X₁ - X₂ ≪[max C₁ C₂] Y₁ + Y₂ := by
+lemma sub_ll_add {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {X₁ X₂:E} {Y₁ Y₂:ℝ} (h1: X₁ ≪[C₁] Y₁) (h2: X₂ ≪[C₂] Y₂) : X₁ - X₂ ≪[max C₁ C₂] Y₁ + Y₂ := by
   rw [ll_def] at h1 h2 ⊢
   apply (norm_sub_le X₁ X₂).trans
   apply (add_le_add h1 h2).trans
   rw [mul_add]
   apply add_le_add
-  . exact mul_le_mul_of_nonneg_right (le_max_left C₁ C₂) hY₁
-  exact mul_le_mul_of_nonneg_right (le_max_right C₁ C₂) hY₂
+  . exact mul_le_mul_of_nonneg_right (le_max_left C₁ C₂) (nonneg_of_ll h1)
+  exact mul_le_mul_of_nonneg_right (le_max_right C₁ C₂) (nonneg_of_ll h2)
 
-lemma mul_ll_mul {C₁ C₂:NNReal} {X₁ X₂:k} {Y₁ Y₂:ℝ} (hY₁: 0 ≤ Y₁) (h1: X₁ ≪[C₁] Y₁) (h2: X₂ ≪[C₂] Y₂) : X₁*X₂ ≪[C₁ * C₂] Y₁ * Y₂ := by
+instance {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] : NeZero (C₁ * C₂) where
+  out := zero_lt_iff.mp (mul_pos (NeZero.pos C₁) (NeZero.pos C₂))
+
+lemma mul_ll_mul {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {X₁ X₂:k} {Y₁ Y₂:ℝ} (h1: X₁ ≪[C₁] Y₁) (h2: X₂ ≪[C₂] Y₂) : X₁*X₂ ≪[C₁ * C₂] Y₁ * Y₂ := by
+  have := nonneg_of_ll h1;
   simp at h1 h2 ⊢
   convert mul_le_mul h1 h2 (norm_nonneg _) (by positivity) using 1
   ring
 
-lemma mul_ll_mul' {C₁ C₂:NNReal} {a:ℝ} {X:V} {Y Z:ℝ} (hY: 0 ≤ Y) (h1: a ≪[C₁] Y) (h2: X ≪[C₂] Z) : a • X ≪[C₁ * C₂] Y * Z := by
+lemma mul_ll_mul' {C₁ C₂:NNReal} [NeZero C₁] [NeZero C₂] {a:ℝ} {X:V} {Y Z:ℝ} (h1: a ≪[C₁] Y) (h2: X ≪[C₂] Z) : a • X ≪[C₁ * C₂] Y * Z := by
+  have := nonneg_of_ll h1
   simp at h1 h2 ⊢
   rw [norm_smul]
   convert mul_le_mul h1 h2 (norm_nonneg _) (by positivity) using 1
   ring
 
-lemma mul_ll_mul_left {C:NNReal} {X:k} {Y:ℝ} (h: X ≪[C] Y) (a:k): a*X ≪[C] ‖a‖ * Y := by
-  rw [<-one_mul C]
-  exact mul_ll_mul (norm_nonneg _) (ll_abs a) h
+lemma mul_ll_mul_left {C:NNReal} [NeZero C] {X:k} {Y:ℝ} (h: X ≪[C] Y) (a:k): a*X ≪[C] ‖a‖ * Y := by
+  apply ll_increase_const _ (show 1*C ≤ C by simp)
+  exact mul_ll_mul (ll_abs a) h
 
-lemma mul_ll_mul_left' {C:NNReal} {X:V} {Y:ℝ} (h: X ≪[C] Y) (a:ℝ): a • X ≪[C] ‖a‖ * Y := by
-  rw [<-one_mul C]
-  exact mul_ll_mul' (norm_nonneg _) (ll_abs a) h
+lemma mul_ll_mul_left' {C:NNReal} [NeZero C] {X:V} {Y:ℝ} (h: X ≪[C] Y) (a:ℝ): a • X ≪[C] ‖a‖ * Y := by
+  apply ll_increase_const _ (show 1*C ≤ C by simp)
+  exact mul_ll_mul' (ll_abs a) h
 
-lemma mul_ll_mul_right {C:NNReal} {X:k} {Y:ℝ} (h: X ≪[C] Y) (a:k) : X*a ≪[C] Y * ‖a‖ := by
+lemma mul_ll_mul_right {C:NNReal} [NeZero C] {X:k} {Y:ℝ} (h: X ≪[C] Y) (a:k) : X*a ≪[C] Y * ‖a‖ := by
   rw [mul_comm X _, mul_comm Y _]
   exact mul_ll_mul_left h a
 
@@ -117,12 +143,12 @@ lemma add_ll_max {Y₁ Y₂:ℝ} (hY₁: 0 ≤ Y₁) (hY₂: 0 ≤ Y₂) : Y₁ 
 
 open BigOperators
 
-lemma sum_ll_sum {C:NNReal} {Ω :Type*} {S: Finset Ω} {X: Ω → E} {Y: Ω → ℝ} (h: ∀ s ∈ S, X s ≪[C] Y s) : (∑ s in S, X s) ≪[C] ∑ s in S, Y s := by
+lemma sum_ll_sum {C:NNReal} [NeZero C] {Ω :Type*} {S: Finset Ω} {X: Ω → E} {Y: Ω → ℝ} (h: ∀ s ∈ S, X s ≪[C] Y s) : (∑ s in S, X s) ≪[C] ∑ s in S, Y s := by
   simp at h ⊢
   rw [Finset.mul_sum]
   exact (norm_sum_le S X).trans (Finset.sum_le_sum h)
 
-lemma tsum_ll_tsum {C:NNReal} {Ω :Type*} [CompleteSpace E] {X: Ω → E} {Y: Ω → ℝ} (h: ∀ s, X s ≪[C] Y s) (hsum: Summable Y): Summable X ∧ ((∑' s, X s) ≪[C] ∑' s, Y s) := by
+lemma tsum_ll_tsum {C:NNReal} [NeZero C] {Ω :Type*} [CompleteSpace E] {X: Ω → E} {Y: Ω → ℝ} (h: ∀ s, X s ≪[C] Y s) (hsum: Summable Y): Summable X ∧ ((∑' s, X s) ≪[C] ∑' s, Y s) := by
   simp at h ⊢
   have : Summable (fun s ↦ ‖X s‖ ) := by
     apply Summable.of_norm_bounded (fun s ↦ C * (Y s)) (Summable.mul_left (C:ℝ) hsum)
@@ -136,35 +162,33 @@ lemma tsum_ll_tsum {C:NNReal} {Ω :Type*} [CompleteSpace E] {X: Ω → E} {Y: Ω
 
 notation:10 X " =[" C "] " Y " + O(" Z ")" => Ll C (X-Y) Z
 
-lemma eqPlusBigO_def (C:NNReal) (X Y:E) (Z:ℝ) : (X =[C] Y + O(Z)) ↔ (‖X-Y‖ ≪[C] Z) := by simp
+lemma eqPlusBigO_def (C:NNReal) [NeZero C] (X Y:E) (Z:ℝ) : (X =[C] Y + O(Z)) ↔ (‖X-Y‖ ≪[C] Z) := by simp
 
-lemma eqPlusBigO_rfl (C:NNReal) (X:E) {Z:ℝ} (hZ: 0 ≤ Z) : (X =[C] X + O(Z)) := by
+lemma eqPlusBigO_rfl (C:NNReal) [NeZero C] (X:E) {Z:ℝ} (hZ: 0 ≤ Z) : (X =[C] X + O(Z)) := by
   simp; positivity
 
-lemma eqPlusBigO_symm (C:NNReal) (X Y:E) {Z:ℝ} : (X =[C] Y + O(Z)) ↔ (Y =[C] X + O(Z)) := by
+lemma eqPlusBigO_symm (C:NNReal) [NeZero C] (X Y:E) {Z:ℝ} : (X =[C] Y + O(Z)) ↔ (Y =[C] X + O(Z)) := by
   simp [<-dist_eq_norm_sub]
   rw [dist_comm]
 
-lemma eqPlusBigO_trans {C C':NNReal} {X Y Z:E} {W:ℝ} (h: X =[C] Y + O(W)) (h': Y =[C'] Z + O(W)) : (X =[C+C'] Z + O(W)) := by
+lemma eqPlusBigO_trans {C C':NNReal} [NeZero C] [NeZero C'] {X Y Z:E} {W:ℝ} (h: X =[C] Y + O(W)) (h': Y =[C'] Z + O(W)) : (X =[C+C'] Z + O(W)) := by
   simp [<-dist_eq_norm_sub] at h h' ⊢
   apply (dist_triangle X Y Z).trans
   convert add_le_add h h' using 1
   ring
 
-lemma eqPlusBigO_trans' {C C':NNReal} (X Y Z:E) {W W':ℝ} (hW: 0 ≤ W) (hW': 0 ≤ W') (h: X =[C] Y + O(W)) (h': Y =[C'] Z + O(W')) : (X =[max C C'] Z + O(W + W')) := by
+lemma eqPlusBigO_trans' {C C':NNReal} [NeZero C] [NeZero C'] (X Y Z:E) {W W':ℝ} (h: X =[C] Y + O(W)) (h': Y =[C'] Z + O(W')) : (X =[max C C'] Z + O(W + W')) := by
   rw [eqPlusBigO_def, <-dist_eq_norm_sub] at h h' ⊢
-  exact ll_of_le_of_ll' (dist_nonneg) (dist_triangle X Y Z) (add_ll_add hW hW' h h')
+  exact ll_of_le_of_ll' (dist_nonneg) (dist_triangle X Y Z) (add_ll_add h h')
 
-lemma add_eqPlusBigO {C: NNReal} (X:E) {Y:E} {Z:ℝ} (h: Y ≪[C] Z) : X + Y =[C] X + O(Z) := by
+lemma add_eqPlusBigO {C: NNReal} [NeZero C] (X:E) {Y:E} {Z:ℝ} (h: Y ≪[C] Z) : X + Y =[C] X + O(Z) := by
   simp at h ⊢
   exact h
 
-lemma sub_eqPlusBigO {C: NNReal} (X:E) {Y:E} {Z:ℝ} (h: Y ≪[C] Z) : X - Y =[C] X + O(Z) := by
+lemma sub_eqPlusBigO {C: NNReal} [NeZero C] (X:E) {Y:E} {Z:ℝ} (h: Y ≪[C] Z) : X - Y =[C] X + O(Z) := by
   simp at h ⊢
   exact h
 
-lemma sum_of_eqPlusBigO {C:NNReal} {Ω :Type*} {S: Finset Ω} {X Y: Ω → E} {Z: Ω → ℝ} (h: ∀ s ∈ S, X s=[C] Y s + O(Z s)) : (∑ s in S, X s) =[C] ∑ s in S, Y s + O(∑ s in S, Z s):= by
+lemma sum_of_eqPlusBigO {C:NNReal} [NeZero C] {Ω :Type*} {S: Finset Ω} {X Y: Ω → E} {Z: Ω → ℝ} (h: ∀ s ∈ S, X s=[C] Y s + O(Z s)) : (∑ s in S, X s) =[C] ∑ s in S, Y s + O(∑ s in S, Z s):= by
   rw [eqPlusBigO_def, <-Finset.sum_sub_distrib, <-norm_ll_iff]
   apply sum_ll_sum h
-
-
