c68397a332a458e2bdabf6da1acde5a7b580a9b2
teorth <tao@math.ucla.edu>
2023-12-25 21:53:20 -0800

    floor function

--- Asymptotic/elementary.lean
+++ Asymptotic/elementary.lean
@@ -1,4 +1,10 @@
 import Asymptotic.majorize
 
 lemma floor_eqPlusBigO {x: ℝ} (hx: 0 ≤ x) : Nat.floor x =[1] x + O(1) := by
-  simp
+  simp [abs_le]
+  have := Nat.floor_eq_iff (n := ⌊x⌋₊) hx
+  simp at this
+  rcases this with ⟨h₁, h₂⟩
+  constructor <;> linarith
+
+
