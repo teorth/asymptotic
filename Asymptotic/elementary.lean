4c2dd360bda29bcbdebe74d46073be562a121fc8
teorth <tao@math.ucla.edu>
2024-01-02 15:05:50 -0800

    ftoc

--- Asymptotic/elementary.lean
+++ Asymptotic/elementary.lean
@@ -3,6 +3,9 @@ import Mathlib.MeasureTheory.Measure.Lebesgue.Basic
 import Mathlib.Data.Set.Card
 import Mathlib.MeasureTheory.Integral.Bochner
 import Mathlib.Analysis.Calculus.Deriv.Basic
+import Mathlib.MeasureTheory.Integral.SetIntegral
+import Mathlib.MeasureTheory.Integral.IntervalIntegral
+import Mathlib.MeasureTheory.Integral.FundThmCalculus
 
 lemma Nat.floor_eqPlusBigO {x: ℝ} (hx: 0 ≤ x) : ⌊x⌋₊ =[1] x + O(1) := by
   simp [abs_le]
@@ -101,15 +104,89 @@ variable {E : Type*} [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E
 
 open BigOperators MeasureTheory
 
-lemma ftoc {a b c:ℝ} (h: a ≤ b) (f: ℝ → E) (hcont: ContinuousOn f (Set.Icc a b))  (hderiv: DifferentiableOn ℝ f (Set.Ioo a b)) (hcont': ContinuousOn (deriv f) (Set.Icc a b))
- (hc: c ∈ Set.Icc a b) : ∀ x ∈ Set.Icc a b, f x = f c + ∫ t in Set.Ioo a b, (if x ≥ c then (if x ≥ t then 1 else 0) else -(if x ≤ t then 1 else 0)) • deriv f t ∂ volume := by
-  sorry
+lemma ftoc {a b c:ℝ} (h: a ≤ b) (f: ℝ → E) (hcont: ContinuousOn f (Set.Icc a b))  (hderiv: ∀ t ∈ Set.Icc a b, DifferentiableAt ℝ f t) (hcont': ContinuousOn (deriv f) (Set.Icc a b))
+ (hc: c ∈ Set.Icc a b) : ∀ x ∈ Set.Icc a b, f x = f c + ∫ t in Set.Icc a b, (if t ≥ c then (if x ≥ t then 1 else 0) else -(if x ≤ t then 1 else 0)) • deriv f t ∂ volume := by
+  intro x hx
+  simp at hc
+  rcases le_or_gt c x with h | h
+  . simp [h]
+    rw [set_integral_eq_of_subset_of_ae_diff_eq_zero (s := Set.Ioc c x)]
+    . rw [set_integral_congr (g := deriv f)]
+      . rw [<- intervalIntegral.integral_of_le h, intervalIntegral.integral_deriv_eq_sub' f (by rfl) _ _]
+        . abel
+        . intro t ht
+          apply hderiv t
+          simp [h] at ht hx ⊢
+          exact ⟨ hc.1.trans ht.1, ht.2.trans hx.2⟩
+        apply ContinuousOn.mono hcont'
+        intro t ht
+        simp [h] at ht hx ⊢
+        exact ⟨ hc.1.trans ht.1, ht.2.trans hx.2 ⟩
+      . exact measurableSet_Ioc
+      intro t ht
+      simp at ht
+      simp [LT.lt.le ht.1, ht.2]
+    . exact MeasurableSet.nullMeasurableSet measurableSet_Icc
+    . simp at hx
+      intro t; simp
+      intro h1 h2; exact ⟨ by linarith only [hc.1, h1], by linarith only [hx.2, h2] ⟩
+    rw [ae_iff, measure_mono_null (s₂ := {c} ∪ {x})]
+    . intro t ht
+      simp at ht ⊢
+      by_cases h' : c ≤ t
+      . simp [h'] at ht
+        rcases le_iff_lt_or_eq.mp h' with h' | h'
+        . exact Or.inl (by linarith only [ht.1 h', ht.2.2.2.1])
+        exact Or.inr h'.symm
+      simp [h'] at ht
+      contrapose! h'
+      exact h.trans ht.2.2.2.1
+    exact measure_union_null Real.volume_singleton Real.volume_singleton
+  simp [h]
+  rw [set_integral_eq_of_subset_of_ae_diff_eq_zero (s := Set.Ioc x c)]
+  . rw [set_integral_congr_ae (g := -deriv f)]
+    . change f x = f c + ∫ (x : ℝ) in Set.Ioc x c, - (deriv f x)
+      rw [MeasureTheory.integral_neg, <- intervalIntegral.integral_of_le (LT.lt.le h), intervalIntegral.integral_deriv_eq_sub' f (by rfl) _ _]
+      . abel
+      . intro t ht
+        apply hderiv t
+        simp [LT.lt.le h] at ht hx ⊢
+        exact ⟨ hx.1.trans ht.1, ht.2.trans hc.2 ⟩
+      apply ContinuousOn.mono hcont'
+      intro t ht
+      simp [LT.lt.le h] at ht hx ⊢
+      exact ⟨ hx.1.trans ht.1, ht.2.trans hc.2 ⟩
+    . exact measurableSet_Ioc
+    rw [ae_iff, measure_mono_null (s₂ := {c})]
+    . intro t ht
+      simp at ht ⊢
+      rcases le_iff_lt_or_eq.mp ht.1 with h' | h'
+      . simp [not_le.mpr h'] at ht
+        linarith [ht.2.1, ht.2.2.1]
+      exact h'
+    exact Real.volume_singleton
+  . exact MeasurableSet.nullMeasurableSet measurableSet_Icc
+  . simp at hx
+    intro t; simp
+    intro h1 h2; exact ⟨ by linarith only [hx.1, h1], ht.2.trans hc.2 ⟩
+  rw [ae_iff, measure_mono_null (s₂ := {c} ∪ {x})]
+  . intro t ht
+    simp at ht ⊢
+    by_cases h' : c ≤ t
+    . simp [h'] at ht
+      rcases le_iff_lt_or_eq.mp h' with h' | h'
+      . linarith only [h', ht.2.2.2.1, h]
+      exact Or.inr h'.symm
+    simp [h'] at ht
+    contrapose! h'
+    exact LT.lt.le (ht.1 ((Ne.le_iff_lt h'.1.symm).mp ht.2.2.2.1))
+
 
 open Classical
 
 noncomputable def discretize (I: Set ℝ) : Finset ℤ := if h : Set.Finite {n:ℤ | (n:ℝ) ∈ I } then h.toFinset else ∅
 
 
-lemma sum_approx_eq_integral {a b c:ℝ} (h: a ≤ b) (f: ℝ → E) (hcont: ContinuousOn f (Set.Icc a b))  (hderiv: DifferentiableOn ℝ f (Set.Ioo a b)) (hcont': ContinuousOn (deriv f) (Set.Icc a b)) (hc: c ∈ Set.Icc a b) (I: Set ℝ) (hI: Set.Ioo a b ⊆ I) (hI': I ⊆ Set.Icc a b) : ∑ n in discretize I, f n =[1] ∫ t in I, f t ∂ volume + O( ‖f c‖ + ∫ t in I, ‖deriv f t‖ ∂ volume) := by
+lemma sum_approx_eq_integral {a b c:ℝ} (h: a ≤ b) (f: ℝ → E) (hcont: ContinuousOn f (Set.Icc a b))  (hderiv: ∀ t ∈ Set.Icc a b, DifferentiableAt ℝ f t) (hcont': ContinuousOn (deriv f) (Set.Icc a b)) (hc: c ∈ Set.Icc a b) (I: Set ℝ) (hI: Set.Ioo a b ⊆ I) (hI': I ⊆ Set.Icc a b) : ∑ n in discretize I, f n =[1] ∫ t in I, f t ∂ volume + O( ‖f c‖ + ∫ t in I, ‖deriv f t‖ ∂ volume) := by
   simp
   sorry
